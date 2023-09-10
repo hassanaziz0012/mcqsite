b38fa624907b1b0aa8748cba66b4687a9c5dbe73
Hassan Aziz <hassanaziz0012@gmail.com>
2023-09-10 15:01:46 +0500

    Changes

--- main/views.py
+++ main/views.py
@@ -29,6 +29,13 @@ class TestStatsView(View):
     def get(self, request, test_id: int):
         test = get_object_or_404(Test, pk=test_id)
         test_questions = test.questions.all().values_list('pk', flat=True)
-        test_answers = Answer.objects.filter(question__in=test_questions).values_list('answer', flat=True)
-        counts = Counter(test_answers)
-        return render(request, 'stats_table.html', context={'test_title': test.title, 'counts': dict(counts)})
+        correct_answer_pks_unformatted = [list(question.correct_answers.all().values_list('pk', flat=True)) for question in test.questions.all()]
+        correct_answer_pks = []
+        for pk_list in correct_answer_pks_unformatted:
+            for pk in pk_list:
+                correct_answer_pks.append(pk)
+
+        test_answers = Answer.objects.filter(pk__in=correct_answer_pks).values_list('answer', flat=True)
+        counts = dict(Counter(test_answers))
+        result = dict(sorted(counts.items(), key=lambda item: item[1], reverse=True))
+        return render(request, 'stats_table.html', context={'test_title': test.title, 'counts': result})
