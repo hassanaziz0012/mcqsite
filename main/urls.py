41f158b53a570a1686e2a4ae91412c98b9ffd88a
Hassan Aziz <hassanaziz0012@gmail.com>
2023-09-10 14:40:55 +0500

    Changes

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('take-test/<int:test_id>', TakeTestView.as_view(), name='take-test'),
+    path('test-stats/<int:test_id>', TestStatsView.as_view(), name='test-stats'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from main.models import Question, Answer, Test
+from collections import Counter
 
 # Create your views here.
 class HomeView(View):
@@ -23,3 +24,11 @@ class TakeTestView(View):
 
         return render(request, 'take_test.html', context={'test': test, 'questions': questions})
     
+
+class TestStatsView(View):
+    def get(self, request, test_id: int):
+        test = get_object_or_404(Test, pk=test_id)
+        test_questions = test.questions.all().values_list('pk', flat=True)
+        test_answers = Answer.objects.filter(question__in=test_questions).values_list('answer', flat=True)
+        counts = Counter(test_answers)
+        return render(request, 'stats_table.html', context={'test_title': test.title, 'counts': dict(counts)})
